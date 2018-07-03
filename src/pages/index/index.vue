1ef1ae3081f5c0b649dd41bb71162d4321de72bf
ronin <745957177@qq.com>
2018-07-03 16:42:47 +0800

    并发async   获取用户信息修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -46,6 +46,8 @@
         <div class="num">{{item.rating.average}}</div>
       </div>
     </div>
+    <open-data type="userNickName"></open-data>
+    <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="onGotUserInfo">三生三世</button>
     <!--<a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>-->
   </div>
 </template>
@@ -105,17 +107,12 @@ export default {
         }
       })
     },
-    getUserInfo () {
-      // 调用登录接口
-      wx.login({
-        success: () => {
-          wx.getUserInfo({
-            success: (res) => {
-              this.userInfo = res.userInfo
-            }
-          })
+    onGotUserInfo (e) {
+      console.log(e, '????');
+        if (!e.target.userInfo) {
+        wx.openSetting();
+        return;
         }
-      })
     },
     clickHandle (msg, ev) {
       console.log('clickHandle:', msg, ev)
@@ -124,25 +121,52 @@ export default {
 
   async created () {
     // 调用应用实例的方法获取全局数据
-    this.getUserInfo();
-    this.in_theatersData = await api.in_theaters();
-    this.coming_soonData = await api.coming_soon();
-    this.searchMoive({
-      tag: '动漫'
-    })
-    let params = {
-      token: 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMjUiLCJpYXQiOjE1MzA1MjE5MzB9.65wFHpmRPpCimevhiFvBeNxj0Kv53Lrk-x1_vE0EgFA',
-      sales_id: 125
-    };
+    // wx.openSetting();
+    // let params = {
+    //   token: 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIxMjUiLCJpYXQiOjE1MzA1MjE5MzB9.65wFHpmRPpCimevhiFvBeNxj0Kv53Lrk-x1_vE0EgFA',
+    //   sales_id: 125
+    // };
+    let r1 = api.in_theaters();
+    let r2 = api.coming_soon();
+    let r3 = this.searchMoive({tag: '动漫'});
+    this.in_theatersData = await r1;
+    this.coming_soonData = await r2;
+    await r3;
+    /**
+     * 共有两种async await 并发的方法
+     * 上面使用的是先用变量统一声明请求方法 然后再使用await调用
+     * 下面的是 使用promise.all 方法 数组里传的是需要同步的方法 返回的值也是数组 内容相对应传递的请求方法
+     * 
+     */
+    // let res = await Promise.all([r1, r2, r3]);
+    // this.in_theatersData = res[0];
+    // this.coming_soonData = res[1];
+
     /**
      * 这里看情况 直接调用api里的接口方法也行，省事，不必再vuex 的actions再重新写一遍。
      * 如果要在调用接口的时候用到vuex的 commit和state 就再vuex  的actions 调用一遍。
      */
-    const {data} = await api.getSalesInfo(params);
-    console.log('res====', data);
+    // const {data} = await api.getSalesInfo(params);
+    // console.log('res====', data);
 
-    const r = await this.f_getSalesInfo(params);
-    console.log(r)
+    // const r = await this.f_getSalesInfo(params);
+    // console.log(r)
+    wx.getSetting({
+  success: (res) => {
+      console.log(res);
+      if (!res.authSetting['scope.userInfo']) {
+          console.log('未授权')
+      } else {
+          console.log('已授权')
+      }
+    /*
+     * res.authSetting = {
+     *   "scope.userInfo": true,
+     *   "scope.userLocation": true
+     * }
+     */
+  }
+})
   }
 }
 </script>
